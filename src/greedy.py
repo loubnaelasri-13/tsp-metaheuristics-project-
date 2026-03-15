b0d8568ee8fe780f4fc7c47e816874f22cecd276
loubnaelasri-13 <lou.elasri@edu.umi.ac.ma>
2026-03-15 16:11:42 +0000

    Update greedy.py

--- src/greedy.py
+++ src/greedy.py
@@ -1,1 +1,27 @@
+import random
+from tsp_utils import tour_length
 
+def greedy_tsp(d):
+
+    n = len(d)
+
+    start = random.randint(0, n-1)
+
+    unvisited = list(range(n))
+    unvisited.remove(start)
+
+    tour = [start]
+
+    current = start
+
+    while unvisited:
+
+        next_city = min(unvisited, key=lambda x: d[current][x])
+
+        tour.append(next_city)
+
+        unvisited.remove(next_city)
+
+        current = next_city
+
+    return tour, tour_length(tour, d)
