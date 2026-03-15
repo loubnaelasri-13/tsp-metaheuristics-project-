a9f21acead1b5d899578b384060bd90d172e152f
loubnaelasri-13 <lou.elasri@edu.umi.ac.ma>
2026-03-15 19:37:40 +0000

    Update tsp_utils.py

--- src/tsp_utils.py
+++ src/tsp_utils.py
@@ -2,34 +2,15 @@ import numpy as np
 import random
 
 def read_instance(filename):
+
     with open(filename, 'r') as f:
         text = f.read()
 
     start = text.find("[[")
     end = text.rfind("]]") + 2
-    matrix_str = text[start:end]
+
+    matrix_str = text[start:end].replace(";", "")
 
     d = np.array(eval(matrix_str))
 
     return d
-
-
-def tour_length(tour, d):
-
-    n = len(tour)
-    cost = 0
-
-    for i in range(n):
-        cost += d[tour[i]][tour[(i+1) % n]]
-
-    return cost
-
-
-def swap_neighbor(tour):
-
-    i, j = random.sample(range(len(tour)), 2)
-    new_tour = tour.copy()
-
-    new_tour[i], new_tour[j] = new_tour[j], new_tour[i]
-
-    return new_tour
